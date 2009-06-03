eb5c16b846170f899cda836335fd827905c6437e
SND\jrall_cp <SND\jrall_cp@81e6e737-5a20-4d0f-a97c-cb01297b46e0>
2009-06-03 14:48:35 +0000

    Added the Resolution project.
    git-svn-id: https://wallmaster.svn.codeplex.com/svn@53781 81e6e737-5a20-4d0f-a97c-cb01297b46e0

---
+++ WallpaperChanger/Resolution/DISPLAY_DEVICE.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices;
+
+namespace Resolution {
+	[StructLayout(LayoutKind.Sequential, CharSet = CharSet.Ansi)]
+	public struct DISPLAY_DEVICE {
+		[MarshalAs(UnmanagedType.U4)]
+		public int cb;
+		
+		[MarshalAs(UnmanagedType.ByValTStr, SizeConst = 32)]
+		public string DeviceName;
+
+		[MarshalAs(UnmanagedType.ByValTStr, SizeConst = 128)]
+		public string DeviceString;
+
+		[MarshalAs(UnmanagedType.U4)]
+		public uint StateFlags;
+
+		[MarshalAs(UnmanagedType.ByValTStr, SizeConst = 128)]
+		public string DeviceID;
+
+		[MarshalAs(UnmanagedType.ByValTStr, SizeConst = 128)]
+		public string DeviceKey;
+
+		public static DISPLAY_DEVICE Default {
+			get {
+				DISPLAY_DEVICE dd = new DISPLAY_DEVICE();
+				dd.cb = Marshal.SizeOf(dd);
+				return dd;
+			}
+		}
+	}
+}

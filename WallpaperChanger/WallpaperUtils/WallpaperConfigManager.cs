063d2ed3619f29577abaea992ec37edd24ff4d23
SND\jrall_cp <SND\jrall_cp@81e6e737-5a20-4d0f-a97c-cb01297b46e0>
2009-11-13 23:08:17 +0000

    Added a try-catch around loading a configuration file to support upgrading from the previous version.
    git-svn-id: https://wallmaster.svn.codeplex.com/svn@61332 81e6e737-5a20-4d0f-a97c-cb01297b46e0

--- WallpaperChanger/WallpaperUtils/WallpaperConfigManager.cs
+++ WallpaperChanger/WallpaperUtils/WallpaperConfigManager.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.IO;
 using System.Xml.Serialization;
+using System.Windows.Forms;
 
 namespace WallpaperUtils {
 
@@ -47,22 +48,35 @@ namespace WallpaperUtils {
 		}
 
 		#region Load
+		/// <summary>
+		/// Loads a wallpaper configuration collection from 
+		/// the default location [ApplicationData\WallMaster\WallmasterConfig.xml]
+		/// </summary>
+		/// <returns>Returns WallpaperConfigCollection if config file found. Otherwise
+		/// NULL is returned</returns>
 		public static WallpaperConfigCollection Load() {
 			string p = GetConfigPath();
 			return Load(p);
 		}
-
+		
+		/// <summary>
+		/// Loads a wallpaper configuration collection from 
+		/// the a specified location
+		/// </summary>
+		/// <returns>Returns WallpaperConfigCollection if config file found. Otherwise
+		/// it'll return a default configuration based on the current number of screens.</returns>
 		public static WallpaperConfigCollection Load(string path) {
-			WallpaperConfigCollection cfg = default(WallpaperConfigCollection);
-
-			// Don't try to open a non-existent file.  Just return null.
-			if (File.Exists(path)) {
-				XmlSerializer xs = new XmlSerializer(typeof(WallpaperConfigCollection));
-				using (StreamReader sr = new StreamReader(path)) {
-						cfg = (WallpaperConfigCollection)xs.Deserialize(sr);
+			try {
+				// Don't try to open a non-existent file.  Just return null.
+				if (File.Exists(path)) {
+					XmlSerializer xs = new XmlSerializer(typeof(WallpaperConfigCollection));
+					using (StreamReader sr = new StreamReader(path)) {
+						return (WallpaperConfigCollection)xs.Deserialize(sr);
+					}
 				}
-			}
-			return cfg;
+			} catch (Exception) {}
+			//-- If we can't read the configuration file, simply return null
+			return null;
 		}
 		#endregion
 
